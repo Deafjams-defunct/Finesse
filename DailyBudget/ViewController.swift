c608315e962c2cb572186d0348bd9e79f3af6962
Emma Foster <emmaalexisfoster@gmail.com>
2017-01-08 19:17:23 -0500

    Initial Commit

---
+++ DailyBudget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyBudget
+//
+//  Created by Emma Foster on 1/8/17.
+//  Copyright © 2017 Emma Foster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
